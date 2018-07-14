a72d20cdc5e8a5cd38d36310a32cbc33acab6140
Levente Löki <leventeloki@gmail.com>
2018-07-14 20:42:18 +0200

    Composite DIC config

---
+++ src/DependencyInjection/Factory/Configuration/CompositeDependencyInjectionConfig.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Lencse\Rectum\DependencyInjection\Factory\Configuration;
+
+use Closure;
+
+class CompositeDependencyInjectionConfig implements DependencyInjectionConfig
+{
+
+    /**
+     * @var DependencyInjectionConfig[]
+     */
+    private $configs;
+
+    /**
+     * @param DependencyInjectionConfig[] $configs
+     */
+    public function __construct(array $configs)
+    {
+        $this->configs = $configs;
+    }
+
+    public function bind(): array
+    {
+        return $this->merge(function (DependencyInjectionConfig $config): array {
+            return $config->bind();
+        });
+    }
+
+    public function factory(): array
+    {
+        return $this->merge(function (DependencyInjectionConfig $config): array {
+            return $config->factory();
+        });
+    }
+
+    public function setup(): array
+    {
+        return $this->merge(function (DependencyInjectionConfig $config): array {
+            return $config->setup();
+        });
+    }
+
+    private function merge(Closure $closure): array
+    {
+        return array_reduce(
+            $this->configs,
+            function (array $carry, DependencyInjectionConfig $config) use ($closure): array {
+                return array_merge($carry, $closure->call($this, $config));
+            },
+            []
+        );
+    }
+}
---
+++ test/Unit/Adapter/DependencyInjection/CompositeDependencyInjectionConfigTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Test\Unit\Adapter\DependencyInjection;
+
+use Lencse\Rectum\DependencyInjection\Factory\Configuration\CompositeDependencyInjectionConfig;
+use Lencse\Rectum\DependencyInjection\Factory\Configuration\DependencyInjectionConfig;
+use PHPUnit\Framework\TestCase;
+
+class CompositeDependencyInjectionConfigTest extends TestCase
+{
+
+    public function testComposite()
+    {
+        $config = new CompositeDependencyInjectionConfig([
+            new class implements DependencyInjectionConfig
+            {
+
+                public function bind(): array
+                {
+                    return ['A' => 'a', 'B' => 'b'];
+                }
+
+                public function factory(): array
+                {
+                    return ['A' => 'af'];
+                }
+
+                public function setup(): array
+                {
+                    return ['A' => ['a' => 1]];
+                }
+            },
+            new class implements DependencyInjectionConfig
+            {
+
+                public function bind(): array
+                {
+                    return ['C' => 'c'];
+                }
+
+                public function factory(): array
+                {
+                    return ['B' => 'bf', 'C' => 'cf'];
+                }
+
+                public function setup(): array
+                {
+                    return ['B' => ['b' => 2]];
+                }
+            },
+        ]);
+
+        $this->assertEquals(['A' => 'a', 'B' => 'b', 'C' => 'c'], $config->bind());
+        $this->assertEquals(['A' => 'af', 'B' => 'bf', 'C' => 'cf'], $config->factory());
+        $this->assertEquals(['A' => ['a' => 1], 'B' => ['b' => 2]], $config->setup());
+    }
+}
