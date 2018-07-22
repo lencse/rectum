21eddb77286714a833acb10be96458169f406909
Levente Löki <leventeloki@gmail.com>
2018-07-22 21:33:17 +0200

    Routing configurator

---
+++ src/Framework/Web/Routing/Configuration/RoutingConfig.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lencse\Rectum\Framework\Web\Routing\Configuration;
+
+interface RoutingConfig
+{
+
+    public function routes(): array;
+}
---
+++ src/Framework/Web/Routing/Configuration/RoutingConfigurator.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Lencse\Rectum\Framework\Web\Routing\Configuration;
+
+use function is_array;
+use Lencse\Rectum\Component\Web\Http\HttpMethod;
+use Lencse\Rectum\Component\Web\Routing\Route;
+use Lencse\Rectum\Component\Web\Routing\RouteCollection;
+use Lencse\Rectum\Component\Web\Routing\RouteHandlerPipeline;
+
+class RoutingConfigurator
+{
+
+    /**
+     * @var RoutingConfig
+     */
+    private $config;
+
+    public function __construct(RoutingConfig $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * @psalm-suppress MixedAssignment
+     * @psalm-suppress MixedArrayAccess
+     */
+    public function __invoke(): RouteCollection
+    {
+        $result = [];
+        foreach ($this->config->routes() as $config) {
+            /** @var HttpMethod $method */
+            $method = $config[0];
+            /** @var string $path */
+            $path = $config[1];
+            /** @var array|string $pipeline */
+            $pipeline = $config[2];
+            /** @var string[] $handlerClasses */
+            $handlerClasses = is_array($pipeline) ? $pipeline : [$pipeline];
+            /** @var string[] $parameterFormats */
+            $parameterFormats = $config[3] ?? [];
+            $result[] = new Route(
+                $method,
+                $path,
+                new RouteHandlerPipeline($handlerClasses),
+                $parameterFormats
+            );
+        }
+
+        return new RouteCollection($result);
+    }
+}
