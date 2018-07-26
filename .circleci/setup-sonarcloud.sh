f7930c75784aca98b9702b1dfe11f47dd8cc1c76
Levente Löki <leventeloki@gmail.com>
2018-07-26 23:51:56 +0200

    Fix

--- .circleci/setup-sonarcloud.sh
+++ .circleci/setup-sonarcloud.sh
@@ -6,6 +6,5 @@ SONAR_CLI_DIR=sonar-scanner-3.2.0.1227-linux
 wget https://sonarsource.bintray.com/Distribution/sonar-scanner-cli/$SONAR_CLI_ZIP
 unzip $SONAR_CLI_ZIP
 
-sudo ln -s $SONAR_CLI_DIR/bin/sonar-scanner /usr/local/bin/sonar-scanner
+echo 'export PATH=/path/to/foo/bin:$PATH' >> ~/.bashrc
 
-sonar-scanner --version
