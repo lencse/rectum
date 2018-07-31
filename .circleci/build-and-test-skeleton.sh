9c4a4e4e4ae0677371183f3042c07894910e42cb
Levente Löki <leventeloki@gmail.com>
2018-07-31 22:21:58 +0200

    Build and test skeleton app in CI

---
+++ .circleci/build-and-test-skeleton.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+RECTUM_DIR=`pwd`
+SKELETON_DIR=/tmp/skeleton
+
+rm -rf $SKELETON_DIR
+
+git clone git@github.com:lencse/rectum-skeleton.git $SKELETON_DIR
+cd $SKELETON_DIR
+
+head -n +1 composer.json > composer.json.1
+echo "\"repositories\": [{\"type\": \"path\", \"url\": \"$RECTUM_DIR\"}]," >> composer.json.2
+tail -n +2 composer.json > composer.json.3
+cat composer.json.1 composer.json.2 composer.json.3 > composer.json
+
+head -n +1 phpunit.xml > phpunit.xml.1
+echo "<php><env name=\"RECTUM_PROJECT_ROOT\" value=\"$SKELETON_DIR\" /></php>" >> phpunit.xml.2
+tail -n +2 phpunit.xml > phpunit.xml.3
+cat phpunit.xml.1 phpunit.xml.2 phpunit.xml.3 > phpunit.xml
+
+cat phpunit.xml
+
+composer install
+
+composer test
