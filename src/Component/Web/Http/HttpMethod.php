8818cde436d9e2d4498b406a33d2ae6b5fa04518
Levente Löki <leventeloki@gmail.com>
2018-07-31 23:34:16 +0200

    Added HTTP methods

--- src/Component/Web/Http/HttpMethod.php
+++ src/Component/Web/Http/HttpMethod.php
@@ -2,6 +2,9 @@
 
 namespace Lencse\Rectum\Component\Web\Http;
 
+/**
+ * @SuppressWarnings(PHPMD)
+ */
 class HttpMethod
 {
     /**
@@ -19,6 +22,11 @@ class HttpMethod
         return $this->method;
     }
 
+    public static function head(): self
+    {
+        return new self('HEAD');
+    }
+
     public static function get(): self
     {
         return new self('GET');
@@ -27,5 +35,40 @@ class HttpMethod
     public static function post(): self
     {
         return new self('POST');
+    }
+
+    public static function put(): self
+    {
+        return new self('PUT');
+    }
+
+    public static function patch(): self
+    {
+        return new self('PATCH');
+    }
+
+    public static function delete(): self
+    {
+        return new self('DELETE');
+    }
+
+    public static function purge(): self
+    {
+        return new self('PURGE');
+    }
+
+    public static function options(): self
+    {
+        return new self('OPTIONS');
+    }
+
+    public static function trace(): self
+    {
+        return new self('TRACE');
+    }
+
+    public static function connect(): self
+    {
+        return new self('CONNECT');
     }
 }
---
+++ test/Unit/Component/Web/Http/HttpMethodTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Test\Unit\Component\Web\Http;
+
+use Lencse\Rectum\Component\Web\Http\HttpMethod;
+use PHPUnit\Framework\TestCase;
+
+class HttpMethodTest extends TestCase
+{
+    public function testHttpMethod()
+    {
+        $methods = [
+            'HEAD',
+            'GET',
+            'POST',
+            'PUT',
+            'PATCH',
+            'DELETE',
+            'PURGE',
+            'OPTIONS',
+            'TRACE',
+            'CONNECT',
+        ];
+        foreach ($methods as $method) {
+            $call = strtolower($method);
+            $this->assertEquals($method, (string) HttpMethod::$call());
+        }
+    }
+}
