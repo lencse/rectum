0aed995d5333e4949a2d13c1b730b51db677441c
Levente Löki <leventeloki@gmail.com>
2018-07-31 20:12:23 +0200

    Default project root path for app testing

--- src/Testing/Facade/AppTesting.php
+++ src/Testing/Facade/AppTesting.php
@@ -13,6 +13,24 @@ class AppTesting
 
     public static function webApplication(): WebAppSetup
     {
-        return new WebAppSetup(self::$projectRoot . '/bootstrap/configuration.php');
+        return new WebAppSetup(self::getProjectRoot() . '/bootstrap/configuration.php');
+    }
+
+    private static function getProjectRoot(): string
+    {
+        return self::$projectRoot ?? self::findProjectRoot();
+    }
+
+    /**
+     * @codeCoverageIgnore
+     */
+    private static function findProjectRoot(): string
+    {
+        $dir = __DIR__;
+        while (!file_exists($dir . '/vendor/lencse/rectum')) {
+            $dir = dirname($dir);
+        }
+
+        return $dir;
     }
 }
